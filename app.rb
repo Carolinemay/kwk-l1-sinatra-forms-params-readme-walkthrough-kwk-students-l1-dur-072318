af6a1f4bb4b92a02eed00b9920819649fe509938
Carolinemay <czhou@students.chccs.k12.nc.us>
2018-07-31 18:59:27 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -7,5 +7,15 @@ class App < Sinatra::Base
   end
 
   # Add your post route and action below
-
+post '/food' do
+  
+  @name=params[:name]
+  @favorite_food= params[:favorite_food]
+  erb :food
+end 
+post '/snacc' do
+  @name= params[:name]
+  @favorite_food= params[:favorite_food]
+  "You, #{@name} are so much of a snacc you're even better than #{@favorite_food}"
+end
 end
